92fd39afc43e4a3db5c9ffb4fbcef601838d175e
clairebookworm <clara32356@gmail.com>
2025-12-14 18:22:48 -0500

    crackly sound but is able to take in tx input rounds

--- controller_1614_ino/controller_1614_ino.ino
+++ controller_1614_ino/controller_1614_ino.ino
@@ -1,33 +1,19 @@
-// ATtiny1614 - sends track control bytes via UART TX
-// upload using megaTinyCore board package
+// ATtiny1614 - append device ID after forwarding upstream data
 
-#define TX_PIN PIN_PA1  // default TX pin for ATtiny1614
-
-// example: 4 buttons to control which tracks play
-#define BTN1 PIN_PA4
-#define BTN2 PIN_PA5
-#define BTN3 PIN_PA6
-#define BTN4 PIN_PA7
+#define DEVICE_ID 0x42  // change per device
 
 void setup() {
-  pinMode(BTN1, INPUT_PULLUP);
-  pinMode(BTN2, INPUT_PULLUP);
-  pinMode(BTN3, INPUT_PULLUP);
-  pinMode(BTN4, INPUT_PULLUP);
-  
-  Serial.begin(9600);  // UART on TX/RX pins
+  Serial.begin(115200);
 }
 
 void loop() {
-  // read button states (active low)
-  bool track1 = !digitalRead(BTN1);
-  bool track2 = !digitalRead(BTN2);
-  bool track3 = !digitalRead(BTN3);
-  bool track4 = !digitalRead(BTN4);
+  // forward everything from upstream first
+  while (Serial.available()) {
+    Serial.write(Serial.read());
+  }
   
-  // pack into single byte: bit0=track1, bit1=track2, etc
-  uint8_t state = (track1 << 0) | (track2 << 1) | (track3 << 2) | (track4 << 3);
+  // then append our id
+  Serial.write(DEVICE_ID);
   
-  Serial.write(state);  // send state byte
-  delay(10);  // send ~100 times/sec
+  delay(10);  // ~100hz
 }
